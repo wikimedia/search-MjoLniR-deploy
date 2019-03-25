2fb503873b93a40262860282d561563a4a5c0ce4
Erik Bernhardson <ebernhardson@wikimedia.org>
2019-03-25 09:36:44 -0700

    Update spark.sh deploy script to use scap script
    This was using scap 'command' type, but that generically
    passes the command into bash requiring us to know where
    it was installed to. Instead use the 'script' type which
    only runs scripts included in the repo's scap/scripts/
    directory.
    
    Additionally update command that copies spark.yaml into place
    to appropriately copy it from the new revision path. This
    almost certainly won't undo the copy if we have to rollback,
    for that we probably need to switch having scap directly
    handle the config deployment.
    
    Change-Id: I04ab375873d96fdf33e44c10f8bca783eda69b7c

--- scap/scripts/spark.sh
+++ scap/scripts/spark.sh
@@ -1,17 +1,19 @@
+#!/bin/bash
+
 set -e
 set -o errexit
 set -o nounset
 set -o pipefail
 
 BASE_DIR="${SCAP_REV_PATH}"
-VENV="${BASE_DIR}/venv"
+VENV_DIR="${BASE_DIR}/venv"
 MJOLNIR_ZIP="${BASE_DIR}/mjolnir_venv.zip"
 
 # Deploy the current configuration for mjolnir's spark utility
 # TODO: Use scap config deployments
-cp "${BASE_DIR}/deploy/spark.yaml" /etc/mjolnir/spark.yaml
+cp "${BASE_DIR}/spark.yaml" /etc/mjolnir/spark.yaml
 
 # Build a .zip of the virtualenv that can be shipped to spark workers if
 # we are on a host with spark installed.
-cd "${VENV}"
+cd "${VENV_DIR}"
 zip -qr "${MJOLNIR_ZIP}" .
