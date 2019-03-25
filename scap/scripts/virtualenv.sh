ba09eb52de23c15fad6c9a61a04a0ac4a58a3890
Erik Bernhardson <ebernhardson@wikimedia.org>
2019-03-25 09:15:39 -0700

    Consistently use VENV_DIR bash variable
    This used VENV is most spots, but VENV_DIR in another. This caused the
    deploy script to fail. To be consistent with the rest of the script
    change the variable to VENV_DIR in all cases.
    
    Change-Id: I5f43a669f709a09e02c7314781df503ba4309e7d

--- scap/scripts/virtualenv.sh
+++ scap/scripts/virtualenv.sh
@@ -7,20 +7,20 @@ set -o pipefail
 
 # The base dir is unique per deployment
 DEPLOY_DIR="${SCAP_REV_PATH}"
-VENV="${DEPLOY_DIR}/venv"
+VENV_DIR="${DEPLOY_DIR}/venv"
 OLD_VENV_DIR="/srv/deployment/search/mjolnir/venv"
 MJOLNIR_DIR="${DEPLOY_DIR}/src"
 WHEEL_DIR="${DEPLOY_DIR}/artifacts"
 REQUIREMENTS="${DEPLOY_DIR}/requirements-frozen.txt"
 
-PIP="${VENV}/bin/pip"
+PIP="${VENV_DIR}/bin/pip"
 
 # Ensure that the virtual environment exists. Don't recreate if already
 # existing, as this will try and downgrade pip on debian jessie from the one
 # installed later which then breaks pip.
 if [ ! -x "$PIP" ]; then
-    mkdir -p "$VENV"
-    virtualenv --never-download --python python3 $VENV || /bin/true
+    mkdir -p "$VENV_DIR"
+    virtualenv --never-download --python python3 $VENV_DIR || /bin/true
 fi
 
 # Debian jessie based hosts need updated versions of pip and wheel or they will
