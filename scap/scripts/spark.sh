728aebc25c0e462c70f4b70cc5068e792a3650b0
Erik Bernhardson <ebernhardson@wikimedia.org>
2019-12-20 16:44:49 -0800

    Drop spark configuration
    This configuration is no longer used. The same purpose is now served
    by the airflow dags in wikimedia/search/analytics repo.
    
    Change-Id: I20edc464bc5f9f7069ebcc1bad40283044dd4ba5

--- scap/scripts/spark.sh
+++ scap/scripts/spark.sh
@@ -9,10 +9,6 @@ BASE_DIR="${SCAP_REV_PATH}"
 VENV_DIR="${BASE_DIR}/venv"
 MJOLNIR_ZIP="${BASE_DIR}/mjolnir_venv.zip"
 
-# Deploy the current configuration for mjolnir's spark utility
-# TODO: Use scap config deployments
-cp "${BASE_DIR}/spark.yaml" /etc/mjolnir/spark.yaml
-
 # Build a .zip of the virtualenv that can be shipped to spark workers if
 # we are on a host with spark installed.
 cd "${VENV_DIR}"
