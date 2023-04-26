5f2ec3509beaba6c9fba62f64a31c44a9719ffac
Erik Bernhardson <ebernhardson@wikimedia.org>
2023-04-26 11:59:19 -0700

    Repoint shebang lines of conda env
    The conda env has all the the shebang lines set to absolute paths on
    the build instance, those wont work for us here. Run a replacement
    over the first line of all the files in the venv bin to replace
    shebangs pointing to the build env with our resulting environmnet.
    
    Change-Id: Ia48b12202b8a43df0d0521536374c118fc8b651c

--- scap/scripts/fetch_conda.sh
+++ scap/scripts/fetch_conda.sh
@@ -15,3 +15,8 @@ ENV_TGZ="${DEPLOY_DIR}/conda_venv.tgz"
 test -f "${ENV_TGZ}" || wget -O "${ENV_TGZ}" -i "${ENV_URL}"
 mkdir "${VENV_DIR}"
 tar -C "${VENV_DIR}" -zxf "${ENV_TGZ}"
+
+# The shebang lines are all absolute, adjust them to our new location
+pattern='^#!.*\/conda_dist_env\/\(bin\/python[0-9]\.[0-9]\+\)$'
+replacement='#!'"${VENV_DIR//\//\\\/}\/\\1"
+sed -i "1 s/${pattern}/${replacement}/" "${VENV_DIR}/bin/*"
